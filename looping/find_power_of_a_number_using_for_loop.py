2f41cc4de5b2f82cf7ae888bb929596a53872901
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-13 23:49:44 +0530

    Create find_power_of_a_number_using_for_loop.py

---
+++ looping/find_power_of_a_number_using_for_loop.py
@@ -0,0 +1,12 @@
+# Write a program to find power of a number using for loop.
+
+def na(n,p):
+    ans=1
+    for i in range(1,p+1):
+        ans=ans*n
+    print(n," power ",p," is ",ans)
+    return 0
+
+n=int(input("Enter n : "))
+p=int(input("Enter p : "))
+na(n,p)
