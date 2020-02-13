c283bddbcf2bcdc77a7e4fe13a3755aa6c90b101
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-13 22:04:05 +0530

    Create print_all_natural_numbers_in_reverse_(from n_to_1).py

---
+++ looping/print_all_natural_numbers_in_reverse_(from n_to_1).py
@@ -0,0 +1,9 @@
+# Write a program to print all natural numbers in reverse (from n to 1).
+
+def na(n):
+    while(n>0):
+        print(n)
+        n=n-1
+    return 0
+n=int(input("Enter : "))
+na(n)
