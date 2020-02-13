0df4857853e323d1435ea6d44362a9e717625f56
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-13 21:58:45 +0530

    Create print_all_natural_numbers_from_1_to_n.py

---
+++ looping/print_all_natural_numbers_from_1_to_n.py
@@ -0,0 +1,13 @@
+# Write a program to print all natural numbers from 1 to n. - using while loop
+
+def na():
+    i=1
+    while(i<=n):
+        print(i)
+        i=i+1
+    return 0
+
+# n is used to get n nos. to be print
+
+n=int(input("Enter : "))
+na()
