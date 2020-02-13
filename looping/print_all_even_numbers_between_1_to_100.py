df65781e41c440960d56e0b90728ed30cf3aad84
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-13 22:17:25 +0530

    Create print_all_even_numbers_between_1_to_100.py

---
+++ looping/print_all_even_numbers_between_1_to_100.py
@@ -0,0 +1,12 @@
+# Write a program to print all even numbers between 1 to 100. - using while loop
+
+def na():
+    i=1
+    while(i<100):
+        if(i%2==0):
+            print(i)
+            i=i+1
+        i=i+1
+    return 0
+
+na()
