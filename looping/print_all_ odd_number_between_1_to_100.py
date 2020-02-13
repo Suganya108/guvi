4d4c1421b36f2a501b36e2e3e22d1677cfa7a26a
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-13 22:20:39 +0530

    Create print_all_ odd_number_between_1_to_100.py

---
+++ looping/print_all_ odd_number_between_1_to_100.py
@@ -0,0 +1,12 @@
+# Write a program to print all odd number between 1 to 100.
+
+def na():
+    i=1
+    while(i<100):
+        if(i%2!=0):
+            print(i)
+            i=i+1
+        i=i+1
+    return 0
+
+na()
