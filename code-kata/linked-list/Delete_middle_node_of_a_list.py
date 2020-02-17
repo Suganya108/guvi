9c8aa7266ab2a214f3195f03cb627a3e33a250bd
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-17 20:35:50 +0530

    Create Delete_middle_node_of_a_list.py

---
+++ code-kata/linked-list/Delete_middle_node_of_a_list.py
@@ -0,0 +1,12 @@
+# Delete the middle node of a linked list and print the resulting linked list.
+
+
+num=int(input())
+n = [int(x) for x in input().split()] 
+m=round(len(n)/2)
+n.pop(m-1)
+for i in range(len(n)):
+    print(n[i],end="")
+    if(i<len(n)-1):
+        print(end=" ")
+               
