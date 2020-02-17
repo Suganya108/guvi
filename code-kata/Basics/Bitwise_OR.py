0dd9d9367aad6ce4443bff225ef29054bfa93303
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-17 16:29:19 +0530

    Create Bitwise_OR.py

---
+++ code-kata/Basics/Bitwise_OR.py
@@ -0,0 +1,8 @@
+# Given a number N and an array of N elements, find the Bitwise OR of the array elements.
+
+n=int(input())
+num=[int(x) for x in input().split()]
+a=num[0]
+for i in range(1,n):
+	a=a | num[i]
+print(a,end="")
