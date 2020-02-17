ce92c4708f8277f5a1f094b43f203281fe0c71ac
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-17 14:17:31 +0530

    Create Odd_or_Even_number.py

---
+++ code-kata/Absolute-beginner/Odd_or_Even_number.py
@@ -0,0 +1,9 @@
+# You are provided with a number check whether its odd or even. 
+
+num=float(input())
+if num==0:
+	print("Zero",end="")
+elif num%2==0:
+	print("Even",end="")
+else:
+	print("Odd",end="")
