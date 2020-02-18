2649e0d400a0df0d0add51a42339b8593de35587
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-18 16:54:21 +0530

    Create Perfect_square.py

---
+++ code-kata/Basics/Perfect_square.py
@@ -0,0 +1,13 @@
+# Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+
+N, M=[int(x) for x in input().split()]
+n=N*M
+sum = 0
+for i in range(1, N+1):
+    n1=i**2
+    if (n1 == n):
+        sum=sum+1
+if(sum>0):
+    print("yes",end="")
+else:
+    print("no",end="")
