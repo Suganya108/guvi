b5bd27a6a47a6e095d02f8ba4c5fc73bcb6d770a
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-19 11:33:45 +0530

    Create Minimum_element_in_a_list.py

---
+++ code-kata/mathematics/Minimum_element_in_a_list.py
@@ -0,0 +1,7 @@
+# Pk finds it difficult to judge the minimum element in the list of elements given to him. 
+# Your task is to develop the algorithm in order to find the minimum element.
+
+num=int(input())
+n = input().split()
+s=min(n)
+print(s,end="")
