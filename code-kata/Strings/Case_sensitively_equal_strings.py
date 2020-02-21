145bedda0caef717a026c885800c3bbec4e36af1
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-21 14:11:42 +0530

    Create Case_sensitively_equal_strings.py

---
+++ code-kata/Strings/Case_sensitively_equal_strings.py
@@ -0,0 +1,8 @@
+# Given 2 strings S1 and s2, check whether they are case senitively equal without using any predefined function(case sensitive).
+# If they are not same print 'no'
+
+s1,s2=input().split()
+if s1==s2:
+  print('yes',end='')
+else:
+  print('no',end='')
