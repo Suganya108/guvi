dc500587c27a1966bd31be5febcc633fb6bb72b8
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-26 12:58:03 +0530

    Update 2.js

--- javascript/activity/2.js
+++ javascript/activity/2.js
@@ -1,2 +1,11 @@
+//print odd nos. in array
+
 a=[1,2,3,4,5,6,7,8,9,10]
 var odd=a.map(function(n){if((n%2)!=0){console.log(n)}})
+
+//print palindromes in array
+
+var arr=['11','121','123','1221','2346']
+var pal=arr.filter(function(n){
+    return n==n.split("").reverse().join("");})
+console.log(pal)
