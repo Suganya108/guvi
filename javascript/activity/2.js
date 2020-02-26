bef3a96d768b854f2152df3159e19dcdb6a1d9ed
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-26 14:01:24 +0530

    Update 2.js

--- javascript/activity/2.js
+++ javascript/activity/2.js
@@ -9,3 +9,13 @@ var arr=['11','121','123','1221','2346']
 var pal=arr.filter(function(n){
     return n==n.split("").reverse().join("");})
 console.log(pal)
+
+//Prime numbers
+
+var a=[1,2,3,4,5,6,7,8,9,10]
+var primeno=a.filter(function(n){
+    for( var i=2;i<n;i++){
+        if(n%i==0) return false
+    }
+    return n!==1})
+console.log(primeno)
