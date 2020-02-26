5aa1fecb88c87c8eb24891213f39d7fc0adf6a68
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-26 12:33:06 +0530

    Update 1.js

--- javascript/activity/1.js
+++ javascript/activity/1.js
@@ -1,23 +1,23 @@
 
-# Convert country name to capital letter using map function
+// Convert country name to capital letter using map function
 
 const conname=dummy.map((item)=>{
     return (item.name).toUpperCase()})
 console.log(conname)
 
-# Filter the datas of country with captital with 5 or less letters
+// Filter the datas of country with captital with 5 or less letters
 
 const cplen=dummy.filter((item)=>{
     return (item.capital).length<=5})
 console.log(cplen)
 
-# Find a data of a country by using find method
+// Find a data of a country by using find method
 
 const finditem=dummy.find((item)=>{
     return item.name==='India'})
 console.log(finditem)
 
-# Sum of all nos. in an array using reduce method
+// Sum of all nos. in an array using reduce method
 
 var a=[1,2,3,4,5,6,7,8,9,10]
 var sum=a.reduce(function(accumulator,currentvalue,index){
@@ -25,7 +25,7 @@ var sum=a.reduce(function(accumulator,currentvalue,index){
 });
 console.log(sum)
 
-# Sum of odd nos. in an array using reduce method
+// Sum of odd nos. in an array using reduce method
 
 var a=[1,2,3,4,5,6,7,8,9,10]
 var sum=a.reduce(function(accumulator,currentvalue,index){
