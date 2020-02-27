f84cc0b82bebc64129c7c50e29b44851710e9c1e
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-27 17:36:39 +0530

    Create Cube_of_a_no.js

---
+++ javascript/codekata/Cube_of_a_no.js
@@ -0,0 +1,12 @@
+//Find the cube of the number.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0];
+  console.log(Math.pow(a,3))
+});
