049b3c173ef81d145821e1721d200829e9528eb9
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-27 13:07:22 +0530

    Create scripts1.js

---
+++ javascript/activity/Simple_DOM_manipulation/scripts1.js
@@ -0,0 +1,13 @@
+//Create 2 div with text "I am div1" & "I am div2" and a button. On click of the button the text of the two divs has to be swapped.
+
+var div = document.createElement("div");
+var input=document.createElement("input")
+document.getElementById("butt").addEventListener("onclick", onclick);
+function swap(){
+    var s=document.getElementById("a");
+    var t=document.getElementById("b");
+    var i=s.innerHTML;
+    var j=t.innerHTML;
+    s.innerHTML=j;
+    t.innerHTML=i;
+}
