a42308186613effe6a2faa8d542b7bbaa3100627
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-27 17:44:51 +0530

    Create meter_centimeter_conversion.js

---
+++ javascript/codekata/meter_centimeter_conversion.js
@@ -0,0 +1,13 @@
+// Convert and print this value in meters and centimeters.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0];
+  console.log(a*1000)
+  console.log(a*100000)
+});
