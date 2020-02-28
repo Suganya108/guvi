e42f7746f51821a70b6c80fe52d33cedd19d030e
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-28 11:22:44 +0530

    Create Remove_whitespaces_&_print_length.js

---
+++ javascript/codekata/Remove_whitespaces_&_print_length.js
@@ -0,0 +1,15 @@
+//Remove all the whitespaces and then print the length of the remaining string.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0],s
+  s=a.split(" "). join("")
+  console.log(s.length)
+  
+  
+});
