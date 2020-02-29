c840c6d2f2c4c1d92b0d3223973f4f34d05913af
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 12:45:47 +0530

    Create product_of_digits_of_no.js

---
+++ javascript/looping/product_of_digits_of_no.js
@@ -0,0 +1,15 @@
+// Write a program to calculate product of digits of a number.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0].split(""),pro=1;
+  for(var i=0;i<n.length;i++){
+      pro*=parseInt(n[i])
+  }
+  console.log(pro)
+});
