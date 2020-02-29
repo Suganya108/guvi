408cb7545b123d9763cb5954e81b56c23a214a7f
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 10:36:27 +0530

    Create print_mul_table_of_n.js

---
+++ javascript/looping/print_mul_table_of_n.js
@@ -0,0 +1,16 @@
+// Write a program to print multiplication table of any number.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0],i=1;
+  while(i<=10){
+      console.log(i+" x "+n+"= "+n*i);
+      i+=1
+      
+  }
+});
