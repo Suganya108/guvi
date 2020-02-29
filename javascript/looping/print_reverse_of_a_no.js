840fe171d9533ccefd599f3b42eeb21cde265b78
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 12:49:34 +0530

    Create print_reverse_of_a_no.js

---
+++ javascript/looping/print_reverse_of_a_no.js
@@ -0,0 +1,15 @@
+// Write a program to enter a number and print its reverse.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0].split(""),s='';
+  for(var i=n.length-1;i>=0;i--){
+      s+=n[i]
+  }
+  console.log(s)
+});
