b3821babef1a99d4d9973c1534463c8f167ffb05
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 10:25:55 +0530

    Create print_sum_of_even_nos.js

---
+++ javascript/looping/print_sum_of_even_nos.js
@@ -0,0 +1,17 @@
+// Write a program to find sum of all even numbers between 1 to n.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0],i=2,sum=0;
+  while(i<=n){
+      sum+=i
+      i+=2;
+  }
+  console.log(sum);
+
+});
