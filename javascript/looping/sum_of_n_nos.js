ceea3773a01e350c12fbe676efef4df22b4b4d9c
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 10:15:32 +0530

    Update sum_of_n_nos.js

--- javascript/looping/sum_of_n_nos.js
+++ javascript/looping/sum_of_n_nos.js
@@ -1,7 +1,14 @@
 // Write a program to find sum of all natural numbers between 1 to n.
 
-var n=6,sum=0;
-for(var i=1;i<=n;i++){
-    sum+=i
-}
-console.log(sum)
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0],sum=0;
+  for(var i=1;i<=n;i++){
+      sum+=i;}
+      console.log(sum);
+});
