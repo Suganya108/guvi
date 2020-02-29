57f31fd6294be61c8fcc1dbab9d471afcd42b07d
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 10:13:54 +0530

    Update print_n_natural_nos_in_reverse.js

--- javascript/looping/print_n_natural_nos_in_reverse.js
+++ javascript/looping/print_n_natural_nos_in_reverse.js
@@ -1,7 +1,16 @@
 // Write a program to print all natural numbers in reverse (from n to 1). - using while loop
 
-var n=6;
-while(n>0){
-    console.log(n)
-    n-=1
-}
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0];
+  while(n>0){
+      console.log(n);
+      n-=1;
+      
+  }
+});
