13a9cae086ed001c273e19b82f1f98298b04795c
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 17:36:38 +0530

    Update prime_no.js

--- javascript/activity/prime_no.js
+++ javascript/activity/prime_no.js
@@ -5,7 +5,7 @@ const n=[];
 inp.on("line",(data)=>{
   n.push(data);});
 inp.on("close",()=>{
-  var a=n[0],pn=[],p1,p2,p3;
+  var a=n[0],pn=[],p1=[],p2=[],p3=[];
   for(var i=2;i<a;i++){
       c=false
       for(var j=2;j<i;j++){
@@ -14,23 +14,22 @@ inp.on("close",()=>{
           }}
           if(c===false){
               pn.push(i);
-              c=0
+              break
               
           }
-  }var t=0
+  }
   for(var i=0;i<pn.length;i++){
       for(var j=i+1;j<pn.length;j++){
           for(var k=j+1;k<pn.length;k++){
               if(pn[i]+pn[j]+pn[k]==a){
-              p1=pn[i];
-              p2=pn[j];
-              p3=pn[k];
-              t+=1
+              p1.push(pn[i]);
+              p2.push(pn[j]);
+              p3.push(pn[k]);
               break;
               }
               
               
-          }if(t==1){console.log(p1,p2,p3);break}
+          }
       }
-  }
+  }console.log(p1[0],p2[0],p3[0])
 });
