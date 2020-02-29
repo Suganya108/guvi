e65bea49fe78a10c5d3becb2bc820a15674441d3
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-02-29 10:19:51 +0530

    Create print_even_nos.js

---
+++ javascript/looping/print_even_nos.js
@@ -0,0 +1,15 @@
+// Write a program to print all even numbers between 1 to 100. - using while loop
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const a=[];
+inp.on("line",(data)=>{
+  a.push(data);});
+inp.on("close",()=>{
+  var n=a[0],i=2;
+  while(i<=n){
+      console.log(i);
+      i+=2;
+  }
+});
