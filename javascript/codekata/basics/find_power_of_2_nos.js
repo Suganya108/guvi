987d74e1257d2d03c99f9b6606ba0180ca73b7f1
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-02 11:10:24 +0530

    Create find_power_of_2_nos.js

---
+++ javascript/codekata/basics/find_power_of_2_nos.js
@@ -0,0 +1,13 @@
+// Given numbers A,B find A^B.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=[];
+  a=n[0].split(" ");
+  console.log(Math.pow(a[0],a[1]));
+  });
