da1fe117cf69c92fd13eca84e343c7fd67e4ecc9
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-02 10:37:15 +0530

    Create form_sides_of_a_triangle.js

---
+++ javascript/codekata/basics/form_sides_of_a_triangle.js
@@ -0,0 +1,16 @@
+// Given 3 numbers A,B,C process and print 'yes' if they can form the sides of a triangle otherwise print 'no'.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=[];
+  a=parseInt(n[0].split(" "));
+  if(a[0]+a[1]>a[2] && a[1]+a[2]>a[0] && a[2]+a[0]>a[1]){
+    console.log("yes")}
+  else{
+    console.log("no")}
+});
