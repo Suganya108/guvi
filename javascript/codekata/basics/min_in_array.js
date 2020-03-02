5deeca34fdbec8b8d4e76fbe40ddd0ac973c4501
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-02 12:06:21 +0530

    Create min_in_array.js

---
+++ javascript/codekata/basics/min_in_array.js
@@ -0,0 +1,13 @@
+// Find the minimum among 10 numbers.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=[];
+  a=n[0].split(" ")
+  console.log(Math.min.apply(null,a));
+});
