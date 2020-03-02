b3ebfceb0708dbb5bde976b25253f4ae605a1fa2
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-02 10:26:40 +0530

    Create perfect_square_or_not.js

---
+++ javascript/codekata/basics/perfect_square_or_not.js
@@ -0,0 +1,23 @@
+// Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=[],s,c=0;
+  a=n[0].split(" ");
+  var N=parseInt(a[0]),M=parseInt(a[1]);
+  var p=N*M;
+  for(var i=1;i<=N;i++){
+    s=Math.pow(i,2);
+    if(s==p)
+      c+=1;
+  }
+  if(c==1)
+    console.log("yes");
+  else
+    console.log("no");
+});
