da0a7efe8a2479418b396f8a06d4b8be9762f5e8
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-02 10:15:54 +0530

    Create bitwise_not_operation.js

---
+++ javascript/codekata/basics/bitwise_not_operation.js
@@ -0,0 +1,12 @@
+// Given a number N, print the Bitwise NOT of that number.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0];
+  console.log(~parseInt(a));
+});
