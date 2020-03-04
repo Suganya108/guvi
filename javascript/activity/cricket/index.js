5fd0b744044982c4407e0dc497d746d8d767e34a
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-04 13:04:51 +0530

    Update index.js

--- javascript/activity/cricket/index.js
+++ javascript/activity/cricket/index.js
@@ -1,42 +1,47 @@
-let s=[],w=[],b=[],o=[],score1=0,score2=0,p=[1,1];
-w=[0,0];b=[36,36];o=[0,0];p=[1,1];
-function play1(){
-    if(w[0]>=10 || b[0]<=0){
-        localStorage.setItem("Score1",score1);
-        alert("Game Over");
-        return 0;
+class cricket{
+    constructor(r1,s1,p1,w1,b1,p,w,b,o,score){
+        this.r1=r1;
+        this.s1=s1;
+        this.p1=p1;
+        this.w1=w1;
+        this.b1=b1;
+        this.p=p;
+        this.w=w;
+        this.b=b;
+        this.o=o;
+        this.score=score;
+        //let p=1,w=0,b=36,o=0,score=0;
+
     }
-    o[0]+=1;
-    let r= Math.floor(Math.random() * 7);
-    document.getElementById("r1").value=r;
-    score1+=parseInt(r);
-    document.getElementById("s1").value=score1;
-    document.getElementById("p1").value=p[0];
-    if(r==0){w[0]+=1;}
-    document.getElementById("w1").value=w[0];
-    let bl1=document.getElementById("b1").value;
-    b[0]=parseInt(bl1)-1;
-    document.getElementById("b1").value=b[0];
-    if(o[0]==6 || r==0){o[0]=0;p[0]+=1;}
     
+    play(){
+        if(this.w>=10 || this.b<=0){
+            alert("Game Over");
+            return this.score;
+        }
+        this.o+=1;
+        let r= Math.floor(Math.random() * 7);
+        document.getElementById(this.r1).value=r;
+        this.score += parseInt(r);
+        document.getElementById(this.s1).value=this.score;
+        document.getElementById(this.p1).value=this.p;
+        if(r==0){this.w+=1;}
+        document.getElementById(this.w1).value=this.w;
+        let bl1=document.getElementById(this.b1).value;
+        this.b=parseInt(bl1)-1;
+        document.getElementById(this.b1).value=this.b;
+        if(this.o==6 || r==0){this.o=0;this.p+=1;}
+        //alert(this.score)
+        
+    }
+}
+let a=new cricket("r1","s1","p1","w1","b1",1,0,36,0,0);
+let b=new cricket("r2","s2","p2","w2","b2",1,0,36,0,0);
+function play1(){
+    let n=a.play();
+    if(n!="undefined")localStorage.setItem("Score1",n);
 }
 function play2(){
-    if(w[1]>=10 || b[1]<=0){
-        localStorage.setItem("Score2",score2);
-        alert("Game Over");
-        return 0;
-    }
-    o[1]+=1;
-    let r= Math.floor(Math.random() * 7);
-    document.getElementById("r2").value=r;
-    score2+=parseInt(r);
-    document.getElementById("s2").value=score2;
-    document.getElementById("p2").value=p[1];
-    if(r==0){w[1]+=1;}
-    document.getElementById("w2").value=w[1];
-    let bl1=document.getElementById("b2").value;
-    b[1]=parseInt(bl1)-1;
-    document.getElementById("b2").value=b[1];
-    if(o[1]==6 || r==0){o[1]=0;p[1]+=1;}
-    
+    let m=b.play();
+    if(m!="undefined")localStorage.setItem("Score2",m);
 }
