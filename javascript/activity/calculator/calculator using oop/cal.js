b9259503b066e709a5a8bef3c53d951ef5bc0873
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-04 19:40:45 +0530

    Create cal.js

---
+++ javascript/activity/calculator/calculator using oop/cal.js
@@ -0,0 +1,18 @@
+
+class calculator{
+    constructor(){}
+do(val){
+    document.getElementById("ans").value+=val;
+}
+
+result(){
+    let x = document.getElementById("ans").value;
+    let y = eval(x);
+    document.getElementById("ans").value=y;
+}
+
+clr(){
+    document.getElementById("ans").value="";
+}
+}
+let c = new calculator();
