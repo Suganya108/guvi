a2f37d7d3022518dd739636d2cff79f82857dd26
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-04 19:50:02 +0530

    Create break.py

---
+++ sample/break.py
@@ -0,0 +1,14 @@
+def qn():
+    n=input("Enter a Value : ")
+    if(n=='q'):
+        exit
+    else:
+        qn()
+    return 0
+
+p=1
+while(n==1):
+    n=input("Enter a Value : ")
+    if(n=='q'):
+        break
+
