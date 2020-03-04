5301fd3fb19fc2cbb23a26656867dce5ef42544a
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-04 19:58:49 +0530

    Create prime_non_prime_nos.js

---
+++ sample/prime_non_prime_nos.js
@@ -0,0 +1,18 @@
+# To Print Prime numbers in a list and non-prime numbers in another list from the given numbers
+
+n=int(input("Enter no. of elements : "))
+pn=[]
+npn=[]
+for i in range(0,n):
+    a=int(input("Enter : "))
+    if(a>1):
+        for i in range(2,a//2+1):
+            if((a%i)==0):
+                print("NPN")
+                npn.append(a)
+                break
+        else:
+            print("PN")
+            pn.append(a)
+print("Prime Number List : ",pn)
+print("Non-Prime Number List : ",npn)
