ac71abf232c585347d942d70b6df5d2df2796506
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-06 18:16:56 +0530

    Create warm_up.js

---
+++ javascript/activity/warm_up.js
@@ -0,0 +1,7 @@
+//Write a function called “isEven”.
+//Given a number, “isEven” returns whether it is even.
+function isEven(num){
+ if(num%2==0)console.log("true");
+ else console.log("false");
+}
+isEven(11);
