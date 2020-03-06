887628cf671c607c65566e87fb276185add1dfcb
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-06 16:53:22 +0530

    Update 3.js

--- javascript/activity/3.js
+++ javascript/activity/3.js
@@ -2,3 +2,9 @@
 //Print odd numbers in an array 
 var a=[1,2,4,5,2,8,9];
 (function(){for(var i=0;i<a.length;i++){if((a[i]%2)!=0){console.log(a[i])}}})();
+//Convert all the strings to title caps in a string array
+var a=["raja","bala","ravi","suba","sundar"];
+(function(){
+    for (var i = 0; i < a.length; i++) {
+        console.log(arr[i][0].toUpperCase()+ arr[i].substr(1));
+    }})();
