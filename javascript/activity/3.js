243b805b04af11efd6b7dad2e3d36b4cb4ee5ec8
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-06 16:48:44 +0530

    Create 3.js

---
+++ javascript/activity/3.js
@@ -0,0 +1,4 @@
+//Do the below programs in anonymous function & IIFE
+//Print odd numbers in an array 
+var a=[1,2,4,5,2,8,9];
+(function(){for(var i=0;i<a.length;i++){if((a[i]%2)!=0){console.log(a[i])}}})();
