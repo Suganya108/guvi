27d7c9abc751e33af043ef5fa7b0ac3020fa6a95
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-06 17:04:47 +0530

    Update 3.js

--- javascript/activity/3.js
+++ javascript/activity/3.js
@@ -8,3 +8,7 @@ var a=["raja","bala","ravi","suba","sundar"];
     for (var i = 0; i < a.length; i++) {
         console.log(a[i][0].toUpperCase()+ a[i].substr(1));
     }})();
+//Sum of all numbers in an array
+var a=[1,2,4,5,2,8,9];
+(function(){var sum=0;for(var i=0;i<a.length;i++)sum+=a[i];
+console.log(sum)})();
