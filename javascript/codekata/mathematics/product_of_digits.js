b9ac1c1b27cdfcd99030f912f752a059fc7ef423
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-09 11:30:59 +0530

    Create product_of_digits.js

---
+++ javascript/codekata/mathematics/product_of_digits.js
@@ -0,0 +1,14 @@
+//Given a number N, print the product of the digits.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0].split(""),p=1;
+  for(let i=0;i<a.length;i++)
+    p*=parseInt(a[i]);
+  console.log(p);
+  });
