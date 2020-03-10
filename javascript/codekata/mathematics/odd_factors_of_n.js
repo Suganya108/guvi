3c98a8ecaf9ab0c00844ff2c42db71b19c3e31ba
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-03-10 10:49:49 +0530

    Create odd_factors_of_n.js

---
+++ javascript/codekata/mathematics/odd_factors_of_n.js
@@ -0,0 +1,15 @@
+// Given a number N, print the odd factors for the N.
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=parseInt(n[0]),f=[];
+  for(var i=1;i<=a;i+=2)
+    if(a%i===0)
+      f.push(i);
+  console.log(f.join(" "));
+});
