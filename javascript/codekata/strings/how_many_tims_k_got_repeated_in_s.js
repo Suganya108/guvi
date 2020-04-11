4ed34f6d71da5d018b1c109be3b0aee0a8ba217f
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-04-11 19:17:58 +0530

    Create how_many_tims_k_got_repeated_in_s.js

---
+++ javascript/codekata/strings/how_many_tims_k_got_repeated_in_s.js
@@ -0,0 +1,16 @@
+/* Given a string 'S' and a character 'K', find how many times 
+'K' got repeated in 'S'.If 'K' is not found in 'S' print -1 /
+
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0].split(" "),c=0;
+  for(var i=0;i<a[0].length;i++)
+    if(a[0][i]===a[1])c+=1;
+  if(c!==0)console.log(c);
+  else console.log("-1");
+});
