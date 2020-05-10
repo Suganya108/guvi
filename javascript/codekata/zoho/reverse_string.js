ef9aede26941d0ee377737f7b9e73e1d60b4a842
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-05-10 21:11:31 +0530

    Create reverse_string.js

---
+++ javascript/codekata/zoho/reverse_string.js
@@ -0,0 +1,9 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  console.log(n[0].split(" ").reverse().join(" "));
+});
