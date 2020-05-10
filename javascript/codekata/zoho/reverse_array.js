2fa5f798d230fe0ed2008551b7ae0cd3971a73df
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-05-10 21:09:32 +0530

    Create reverse_array.js

---
+++ javascript/codekata/zoho/reverse_array.js
@@ -0,0 +1,9 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  console.log(n[1].split(" ").reverse().join(" "));
+});
