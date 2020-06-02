a1380570547a0b1e0e21c4e327965365fd78b2d3
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-06-02 21:16:31 +0530

    Create 1.4.js

---
+++ Day 2:(02-06-2020)/1.4.js
@@ -0,0 +1,21 @@
+//1.4 Display the count of all countries sharing their border with the country name in console.
+
+var request = new XMLHttpRequest() //object to request Http connection
+    //To set api connection true
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true)
+
+// function to load http connection
+
+request.onload = function() {
+
+    var jsonData = JSON.parse(this.response) // object to exract json object from api
+
+    // Map Function to extract countries names , borders sharing count and borders sharing
+
+    const conCapital = jsonData.map((item) => {
+        console.log(item.name, "shares its border with", item.borders.length, "countries that includes", item.borders);
+    });
+
+}
+
+request.send();
