3906ed1ba9b32a5813e61988e57030a1019b2fa5
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-06-04 01:42:27 +0530

    Delete 4.1.js

--- Day 3:(03-06-2020)/4.1.js
+++
@@ -1,5 +0,0 @@
-//4.1. Print odd numbers in an array
-var array=[1,2,3,4,5,6,7,8,9,10];
-//arrow function to filter odd numbers
-var oddNumber=array.filter(num=>(num%2)!==0);
-console.log(oddNumber)//[ 1, 3, 5, 7, 9 ]
