3364794ff245531b7be8e3d71243883bbecdb6cc
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-06-04 18:13:26 +0530

    Update 2.js

--- Day 3:(03-06-2020)/2.js
+++ Day 3:(03-06-2020)/2.js
@@ -66,6 +66,20 @@ var palindromeArray=function palindrome(array){
     return newArray}
 console.log(palindromeArray(array))//[ 'eye', 'abba' ]
 
+//2.6 Return median of two sorted arrays of same size
+
+var array1=[1,2,3,4,5,6,7];
+var array2=[4,5,7,8,8,9];
+//anonymous function to find median of 2 array
+var medianOfArray=function median(array1,array2){
+    var newArray=[...array1,...array2],s,len=newArray.length;
+    newArray.sort(((x,y)=>x-y));
+    s=(newArray[len/2]+newArray[len/2+1])/2
+    return 2;
+}
+console.log(medianOfArray(array1,array2))//2
+
+
 //2.7. Remove duplicates from an array
 
 var array=["eye","apple","abba","guvi","geek","apple","abba"];
