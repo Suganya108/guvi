e32a7f7db61294f34b659817aaa22810ad140825
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-06-04 10:51:06 +0530

    Update 2.js

--- Day 3:(03-06-2020)/2.js
+++ Day 3:(03-06-2020)/2.js
@@ -11,3 +11,28 @@ var oddNumber=function findOddNumber(array){
     return newArray;
 };
 console.log(oddNumber(array))//[ 1, 3, 5, 7, 9 ]
+
+//2.2. Convert all the strings to title caps in a string array
+var array=["apple","bat","cat","dog"];
+//anonymous function to Convert all the strings to title caps
+var titleCaps=function convertTitltCaps(word){
+    var arrayString=[];
+    for(var i=0;i<word.length;i++){
+   var string = word[i][0].toUpperCase();
+   for(var j=1;j<word[i].length;j++){
+       string+=word[i][j].toLowerCase();
+    }
+    arrayString.push(string);
+    }
+   return arrayString;
+    
+};
+console.log(titleCaps(array))//[ 'Apple', 'Bat', 'Cat', 'Dog' ]
+
+//4.3. Sum of all numbers in an array
+
+var array=[1,2,3,4,5,6,7,8,9,10];
+//anonymous function to find sum of all numbers in an array
+var sumOfArray=function findSumOfArray(array){
+    return array.reduce((a, b) => a + b)};
+console.log(sumOfArray(array))//55
