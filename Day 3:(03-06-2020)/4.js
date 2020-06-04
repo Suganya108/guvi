22bb2077eda1fa99ce11ae09c67fb7f1e5f5d665
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-06-04 12:42:34 +0530

    Update 4.js

--- Day 3:(03-06-2020)/4.js
+++ Day 3:(03-06-2020)/4.js
@@ -23,3 +23,30 @@ var array=[1,2,3,4,5,6,7,8,9,10];
 //arrow function to find sum of all numbers in an array
 var sumOfArray=array.reduce((a, b) => a + b);
 console.log(sumOfArray)//55
+
+//4.4. Return all the prime numbers in an array
+var array=[1,2,3,4,5,6,7,8,9]
+//arrow function to Return all the prime numbers from an array
+var primeArray=prime((array)=>{
+    var newArray=[];
+    for(var n in array){
+    for(var i=2;i<array[n];i++){
+        var c=1;
+        if(array[n]%i===0){c*=0;break}
+        else c*=1;
+    }
+    if(c===1)newArray.push(array[n]);
+    else if(array[n]===2)newArray.push(array[n]);
+    }
+    return newArray;
+});
+console.log(primeArray(array));
+
+//4.5 Return all the palindromes in an array
+
+var array=["eye","apple","abba","guvi"];
+//arrow function to filter palindrome
+var palindromeArray=array.filter(str=>{
+    var reverseStr=str.split("").reverse().join("");
+    if(str===reverseStr)return str;});
+console.log(palindromeArray)//[ 'eye', 'abba' ]
