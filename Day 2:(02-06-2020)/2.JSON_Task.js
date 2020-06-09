5eaa87959a4e4de7dfb0dbf77f3a972dd572e0c5
Suganya108 <60962487+Suganya108@users.noreply.github.com>
2020-06-09 12:24:36 +0530

    Update 2.JSON_Task.js

--- Day 2:(02-06-2020)/2.JSON_Task.js
+++ Day 2:(02-06-2020)/2.JSON_Task.js
@@ -297,9 +297,9 @@ var securityQuestions = [
 function chksecurityQuestions(securityQuestions,ques,ans) {
     for(var i=0;i<securityQuestions.length;i++){
         if(securityQuestions[i].question===ques && securityQuestions[i].expectedAnswer===ans)return true;
-        else return false;
+        
     }
-
+return false;
 }
 //Test case1:
 var ques = "What was your first pet’s name?";
@@ -307,6 +307,11 @@ var ans  =  "FlufferNutter";
 var status = chksecurityQuestions(securityQuestions, ques, ans);
 console.log(status); // true
 //Test case2:
+var ques = "What was the model year of your first car?";
+var ans  =  "1985";
+var status = chksecurityQuestions(securityQuestions, ques, ans);
+console.log(status); // true
+//Test case3:
 var ques = "What was your first pet’s name?";
 var ans  =  "DufferNutter";
 var status = chksecurityQuestions(securityQuestions, ques, ans);
